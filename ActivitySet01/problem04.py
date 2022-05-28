08ec62222cc1e0a171b9f460063a0739a0d8fada
manish005001 <96286053+manish005001@users.noreply.github.com>
2022-05-28 16:37:46 +0000

    hello word

--- ActivitySet01/problem04.py
+++ ActivitySet01/problem04.py
@@ -1,3 +1,9 @@
 # Conditional Execution
 
-hrs = input("Enter hours? ")
+hrs = input("Enter hours : ")
+pay = input("Enter the rate per hour :")
+if hrs>=40:
+  i=40 , sum=0
+  while i=hrs:
+    print(int(sum+i))
+  print(int(1.5*pay*sum+i))
--- ActivitySet01/problem07.py
+++ ActivitySet01/problem07.py
@@ -1,3 +1,3 @@
-# Strings
+ # Strings
 
 text = "X-DSPAM-Confidence:    0.8475"
