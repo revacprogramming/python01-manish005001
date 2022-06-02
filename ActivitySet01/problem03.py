536f4664df17e3c3bc714c11a8edbec4cb41d251
manish005001 <96286053+manish005001@users.noreply.github.com>
2022-06-02 18:04:10 +0000

    program2

--- ActivitySet01/problem03.py
+++ ActivitySet01/problem03.py
@@ -1,3 +1,17 @@
 # Variables, Expressions & Statements
 
-hrs = float(input("Enter hours? "))
+sum=0
+count=0
+try:
+    while True:
+        number=input("enter the number :")
+        if number=="done":
+            break
+        sum+=int(number)
+        count+=1
+
+    print("the sum of the enter the number is : ",sum)
+    print("total number Enter by the user is : ",count)
+    print("the average of the given number is : ",sum/count)
+except ValueError:
+    print("invalid number")
