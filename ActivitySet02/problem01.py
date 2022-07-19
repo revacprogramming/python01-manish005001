b3ad1b8e2a298d38014c45a278ae9a4514a8627f
manish005001 <96286053+manish005001@users.noreply.github.com>
2022-07-19 17:23:18 +0000

    code14

--- ActivitySet02/problem01.py
+++ ActivitySet02/problem01.py
@@ -2,7 +2,7 @@
 def add(a,b):
     return (int(a)+int(b))
 def main():
-    a=input('Enter the first number :')
+    a=input('enter the first number :')
     b=input("Enter the 2nd number : ")
     c=add(a,b)
     print(c)
